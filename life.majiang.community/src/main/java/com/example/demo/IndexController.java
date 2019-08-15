fbbcf3bf2660932f0631937cb78cd2178d2f6feb
sunweixioaye <542784145@qq.com>
2019-08-15 16:19:49 +0800

    利用bootstrap框架创建了index.html

--- life.majiang.community/src/main/java/com/example/demo/IndexController.java
+++ life.majiang.community/src/main/java/com/example/demo/IndexController.java
@@ -5,10 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 @Controller
-public class HelloController {
-	@GetMapping("/hello")
-	public String hello(@RequestParam(name="name") String name,Model model) {
-		model.addAttribute("name", name);
-		return "hello";
+public class IndexController {
+	@GetMapping("/")
+	public String hello() {		
+		return "index";
 	}
 }
